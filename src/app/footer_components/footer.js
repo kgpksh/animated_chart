49884ef4aac25254bfb8a8dde0526959c3dc306e
kgpksh <tkdgns543@gmail.com>
2024-08-10 21:10:48 +0900

    chore: copyright 적기, 가격 20달러로 변경, 라이센스 포함시키기

--- src/app/footer_components/footer.js
+++ src/app/footer_components/footer.js
@@ -2,6 +2,10 @@ import Link from "next/link"
 
 export default function Footer() {
     return(
-        <div className="text-xs"><Link href={'/terms'}>Terms of Service</Link></div>
+        <div className="flex">
+            <div className="text-xs mr-3"><Link href={'/terms'}>Terms of Service</Link></div>
+            <div className="text-xs">Copyright © 2024 - All rights reserved</div>
+        </div>
+        
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
         <div className="flex w-full h-full items-center justify-center">
           <div className="w-[700px] h-1/2 p-3">
             <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-5">Chart animation tool</h2>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Convert your excel data to custom chart animation</h3>
+            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Convert your excel data to custom short chart animation</h3>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Download mp4 video files to your computer</h3>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Running on your local PC</h3>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- No chart data sending to server</h3>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Unlock tool and features in $15/mo</h3>
+            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">- Unlock tool and features in $20/mo</h3>
             <Button className='mt-6'><Link href={'/chart'}>Move to chart editor</Link></Button>
           </div>
           <div className="w-[800px] h-2/3 items-center">
